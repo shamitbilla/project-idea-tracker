648aba582219ba351be7bda4312121605fc9b49c
Shamit <114471531+shamitbilla@users.noreply.github.com>
2025-06-04 16:05:08 +0530

    Update supabase.ts

--- mobile-app/utils/supabase.ts
+++ mobile-app/utils/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import Constants from 'expo-constants';
 
-const supabaseUrl = "https://akofzoljrfwcflxtubjz.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrb2Z6b2xqcmZ3Y2ZseHR1Ymp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMTA2ODYsImV4cCI6MjA2MzU4NjY4Nn0.lOq2jzlSGkD76zHpJbOHYHnJoYCAGoKNJtXy_Ti-6Xc";
+const supabaseUrl = "";
+const supabaseAnonKey = "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
